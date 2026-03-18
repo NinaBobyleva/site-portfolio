8d060e1a85c913b4d7e36ffa3989ac7f0543a3a9
Nina Bobyleva <2847171@mail.ru>
2026-03-18 20:42:41 +0300

    Установила Gravity UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@gravity-ui/uikit/styles/fonts.css";
+import "@gravity-ui/uikit/styles/styles.css";
+import "./global.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@gravity-ui/uikit";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme="light">
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@gravity-ui/uikit";
+
 export const MainPage = () => {
-    return <h1>Main Page</h1>
-}
+  return (
+    <>
+      <h1>Main Page</h1>
+      <Button view="action" size="xl">Skip</Button>
+    </>
+  );
+};
